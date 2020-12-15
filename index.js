ce7ccbfca5416bcf9cc25ca8f3033bed4d676803
ariebrainware <publicu001@protonmail.com>
2020-12-15 18:28:21 +0700

    Fix script and add basic styling

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const hitung = () =>{
-    var saldo=parseFloat(document.f1922028.isaldo.value);
-    var bunga=parseFloat(document.f1922028.ibunga.value)/100;
-    var waktu=parseFloat(document.f1922028.iwaktu.value);
+function hitung() {
+    var saldo=parseFloat(document.formdata.isaldo.value);
+    var bunga=parseFloat(document.formdata.ibunga.value)/100;
+    var waktu=parseFloat(document.formdata.iwaktu.value);
     var hasil = "";
     for(i=1;i<=waktu;i++)
         {
             saldo=saldo+(saldo*bunga);
-            hasil=hasil+"Saldo Bulan"+i+"=Rp."+saldo+"\n";
+            hasil=hasil+"Saldo Bulan "+i+" = Rp."+saldo+"\n";
         }
-    document.form-data.ohasil.value=hasil;
+    document.formdata.ohasil.value=hasil;
 }
